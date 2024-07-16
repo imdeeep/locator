23972e46eeb0058e87b43014a43bfee53bdef62e
morningstar <mandeep7yadav@gmail.com>
2024-07-16 09:32:28 +0530

    authBlock

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -5,20 +5,20 @@ import Cookies from 'js-cookie';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const token = Cookies.get('token');
-    console.log('Token from cookie:', token);
+  // useEffect(() => {
+  //   const token = Cookies.get('token');
+  //   console.log('Token from cookie:', token);
 
-    if (token) {
-      setAuthenticated(true);
-    } else {
-      setAuthenticated(false);
-    }
-    setLoading(false);
-  }, []);
+  //   if (token) {
+  //     setAuthenticated(true);
+  //   } else {
+  //     setAuthenticated(false);
+  //   }
+  //   setLoading(false);
+  // }, []);
 
   return (
     <AuthContext.Provider value={{ authenticated, setAuthenticated, loading }}>
