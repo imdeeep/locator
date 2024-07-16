0a4331052f15001c5d184189af51fee3335aadd8
morningstar <mandeep7yadav@gmail.com>
2024-07-16 09:01:56 +0530

    demoDeployement

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000", // frontend URL
+    origin: ["http://localhost:3000","*"], // frontend URL
     methods: ["GET", "POST"],
     credentials: true, 
   },
@@ -28,7 +28,7 @@ connectDB();
 
 // Middlewares
 app.use(cors({
-  origin: "http://localhost:3000", // Frontend URL
+  origin: ["http://localhost:3000","*"], // Frontend URL
   credentials: true, 
 }));
 app.use(express.json());
@@ -67,15 +67,36 @@ io.on("connection", (socket) => {
     console.log("User registered:", user);
   });
 
+  // Handle connection requests
+  socket.on("sendConnectionRequest", (toUserId) => {
+    if (users[toUserId]) {
+      io.to(toUserId).emit("connectionRequest", { fromUser: users[socket.id], fromSocketId: socket.id });
+    }
+  });
+
+  // Handle acceptance of connection requests
+  socket.on("acceptConnectionRequest", (fromSocketId) => {
+    if (users[fromSocketId] && users[socket.id]) {
+      users[socket.id].connectedUsers = users[socket.id].connectedUsers || [];
+      users[fromSocketId].connectedUsers = users[fromSocketId].connectedUsers || [];
+      
+      users[socket.id].connectedUsers.push(fromSocketId);
+      users[fromSocketId].connectedUsers.push(socket.id);
+
+      io.to(fromSocketId).emit("connectionAccepted", { user: users[socket.id], socketId: socket.id });
+      socket.emit("connectionAccepted", { user: users[fromSocketId], socketId: fromSocketId });
+    }
+  });
+
   // Update location
   socket.on("updateLocation", (data) => {
     if (users[socket.id]) {
       users[socket.id].lat = data.lat;
+
       users[socket.id].lng = data.lng;
       io.emit("locationUpdate", { ...users[socket.id], id: socket.id });
     }
   });
-
   // Disconnect user
   socket.on("disconnect", () => {
     console.log("Client disconnected");
